3e673d9394a664b449119b73ea245bcdf24d5a6a
Sakib Hasan <sakib100.sa@gmail.com>
2022-08-29 15:22:33 +0600

    responsiveness complete on Gpt Section

--- src/Components/GPT/GPT.js
+++ src/Components/GPT/GPT.js
@@ -24,16 +24,16 @@ const GPT = () => {
 			className="my-10 mx-5 md:mx-10 xl:mx-20 2xl:mx-30"
 			id="what-is-gpt">
 			<div className="bg-[#042c54]   px-[2rem] py-[3rem] relative overflow-hidden">
-				<div className="blur-two" />
-				<div className="mb-20 flex flex-1 flex-col md:flex-row justify-between">
+				<div className="blur-two " />
+				<div className="pt-5 mb-20 flex w-full flex-col md:flex-row justify-between">
 					<div className="relative">
-						<div className="gradient-border absolute top-[-10px]" />
-						<h6 className="text-[2rem] font-[800]">
+						<div className="gradient-border absolute top-[-10px] " />
+						<h6 className="text-[2rem] font-[800] 2xl:text-[3.5rem]">
 							What is GPT-3
 						</h6>
 					</div>
 					<div>
-						<p className="max-w-[550px]">
+						<p className="max-w-[550px] 2xl:max-w-[800px]  2xl:text-[1.3rem]">
 							We so opinion friends me message as delight. Whole
 							front do of plate heard oh ought. His defective nor
 							convinced residence own. Connection has put
@@ -43,24 +43,24 @@ const GPT = () => {
 						</p>
 					</div>
 				</div>
-				<div className="my-10 flex flex-1 flex-col md:flex-row justify-between items-start">
-					<h3 className="gradient text-[2.5rem] font-[800] mb-[5rem]">
+				<div className="my-10 mb-20 2xl:my-[8rem] flex flex-1 flex-col md:flex-row justify-between items-start">
+					<h3 className="gradient text-[2.5rem] 2xl:text-[3.5rem]  font-[800] pb-3">
 						The possibilities are beyond{" "}
 						<br className="hidden md:block" /> your imagination
 					</h3>
-					<p className="text-[#FF8A71] text-[1.5rem]">
+					<p className="text-[#FF8A71] text-[1.5rem] 2xl:text-[2rem]">
 						Explore The Library
 					</p>
 				</div>
 
 				<div className="my-10 flex flex-1 flex-col md:flex-row justify-between items-center">
 					{features.map((feature, index) => (
-						<div className="relative px-3 py-5" key={index}>
+						<div className="relative py-5" key={index}>
 							<div className="gradient-border md:absolute top-[-10px] mb-2 md:mb-0" />
-							<h6 className="text-[2rem] font-[800] mb-10">
+							<h6 className="text-[2rem] font-[800] mb-8 2xl:text-[3rem]">
 								{feature.name}
 							</h6>
-							<p className="max-w-[650px] text-primary text-[1rem] leading-[2rem]font-[500] ">
+							<p className=" text-primary text-[1rem] 2xl:text-[1.5rem] leading-[2rem] font-[500] ">
 								{feature.message}
 							</p>
 						</div>
--- src/Components/OpenAi/OpenAi.js
+++ src/Components/OpenAi/OpenAi.js
@@ -3,34 +3,35 @@ import React from "react";
 const OpenAi = () => {
 	return (
 		<section
-			className="my-10 mx-5 md:mx-10 xl:mx-20 2xl:mx-30"
+			className="my-20 py-10 mx-5 md:mx-10 xl:mx-20 2xl:mx-30"
 			id="open-ai">
-			<div className="flex flex-col lg:flex-row justify-between items-start ">
-				<div className="pr-5">
-					<h3 className="gradient text-[2.5rem] font-[800] mb-10">
+			<div className="flex flex-col  md:flex-row lg:justify-between items-start ">
+				<div className="md:pr-10">
+					<h3 className="gradient text-[2rem] 2xl:text-[3rem] font-[800] mb-10">
 						The Future is Now and <br className="hidden md:block" />{" "}
 						You Just Need To Realize{" "}
-						<br className="hidden md:block" /> It. Step into Future
+						<br className="hidden md:block " /> It. Step into Future
 						Today <br className="hidden md:block" /> & Make it
 						Happen.
 					</h3>
 
-					<p className="text-[#FF8A71] font-[500] text-[1rem] leading-[30px] ">
+					<p className="text-[#FF8A71] font-[500] text-[1rem] leading-[30px] cursor-pointer mb-10">
 						Request Early Access to Get Started
 					</p>
 				</div>
 
-				<div className="flex flex-1  flex-col   items-center px-3 py-5">
-					<div className=" w-full flex flex-col lg:flex-row justify-between lg:items-center mb-5">
+				<div className="flex flex-1  flex-col   items-center">
+					<div className=" w-full flex flex-col lg:flex-row justify-between lg:items-center mb-10">
 						<div className="relative">
-							<div className="gradient-border md:absolute top-[-10px] mb-2 md:mb-0" />
-							<h6 className="text-[1.2rem] font-[800] leading-[24px] tracking-[-0.04em] mb-2 ">
-								Improving end <br className="hidden lg:block" />{" "}
+							<div className="gradient-border w-[20%] py-[2px] md:absolute top-[-10px] mb-2 md:mb-0" />
+							<h6 className="text-[1.2rem] 2xl:text-[1.5rem]  font-[800] leading-[24px] tracking-[-0.04em] mb-2 ">
+								Improving end{" "}
+								<br className="hidden lg:block 2xl:hidden" />{" "}
 								distrusts instantly{" "}
 							</h6>
 						</div>
 						<div>
-							<p className="max-w-[335px] text-primary text-[1rem] font-[500] leading-[1.5rem]">
+							<p className="max-w-[335px] xl:max-w-[450px]  2xl:max-w-[550px] text-primary text-[1rem] 2xl:text-[1.2rem] font-[500] leading-[1.5rem]">
 								From they fine john he give of rich he. They age
 								and draw mrs like. Improving end distrusts may
 								instantly was household applauded.
@@ -38,16 +39,17 @@ const OpenAi = () => {
 						</div>
 					</div>
 
-					<div className=" w-full flex flex-col lg:flex-row justify-between lg:items-center mb-5">
+					<div className=" w-full flex flex-col lg:flex-row justify-between lg:items-center mb-10">
 						<div className="relative">
 							<div className="gradient-border lg:absolute top-[-10px] mb-2 md:mb-0" />
-							<h6 className="text-[1.2rem] font-[800] leading-[24px] tracking-[-0.04em] mb-2">
+							<h6 className="text-[1.2rem] 2xl:text-[1.5rem] font-[800] leading-[24px] tracking-[-0.04em] mb-2">
 								Become the
-								<br className="hidden lg:block" /> tended active
+								<br className="hidden lg:block 2xl:hidden" />{" "}
+								tended active
 							</h6>
 						</div>
 						<div>
-							<p className="max-w-[335px] text-primary text-[1rem] font-[500] leading-[1.5rem]">
+							<p className="max-w-[335px] xl:max-w-[450px]  2xl:max-w-[550px] text-primary text-[1rem] 2xl:text-[1.2rem] font-[500] leading-[1.5rem]">
 								Considered sympathize ten uncommonly occasional
 								assistance sufficient not. Letter of on become
 								he tended active enable to.
@@ -55,16 +57,17 @@ const OpenAi = () => {
 						</div>
 					</div>
 
-					<div className=" w-full flex flex-col lg:flex-row justify-between lg:items-center mb-5">
+					<div className=" w-full flex flex-col lg:flex-row justify-between lg:items-center mb-10">
 						<div className="relative">
 							<div className="gradient-border lg:absolute top-[-10px] mb-2 md:mb-0" />
-							<h6 className="text-[1.2rem] font-[800] leading-[24px] tracking-[-0.04em] mb-2">
+							<h6 className="text-[1.2rem] 2xl:text-[1.5rem] font-[800] leading-[24px] tracking-[-0.04em] mb-2">
 								Message or am
-								<br className="hidden lg:block" /> nothing
+								<br className="hidden lg:block 2xl:hidden" />{" "}
+								nothing
 							</h6>
 						</div>
 						<div>
-							<p className="max-w-[335px] text-primary text-[1rem] font-[500] leading-[1.5rem]">
+							<p className="max-w-[335px] xl:max-w-[450px]  2xl:max-w-[550px] text-primary text-[1rem] 2xl:text-[1.2rem] font-[500] leading-[1.5rem]">
 								Led ask possible mistress relation elegance eat
 								likewise debating. By message or am nothing
 								amongst chiefly address.
@@ -72,16 +75,17 @@ const OpenAi = () => {
 						</div>
 					</div>
 
-					<div className=" w-full flex flex-col lg:flex-row justify-between lg:items-center mb-5">
+					<div className=" w-full flex flex-col lg:flex-row justify-between lg:items-center mb-10">
 						<div className="relative">
 							<div className="gradient-border lg:absolute top-[-10px] mb-2 md:mb-0" />
-							<h6 className="text-[1.2rem] font-[800] leading-[24px] tracking-[-0.04em] mb-2">
+							<h6 className="text-[1.2rem] 2xl:text-[1.5rem] font-[800] leading-[24px] tracking-[-0.04em] mb-2">
 								Really boy law
-								<br className="hidden lg:block" /> county
+								<br className="hidden lg:block 2xl:hidden" />{" "}
+								county
 							</h6>
 						</div>
 						<div>
-							<p className="max-w-[335px] text-primary text-[1rem] font-[500] leading-[1.5rem]">
+							<p className="max-w-[335px] xl:max-w-[450px]  2xl:max-w-[550px] text-primary text-[1rem] 2xl:text-[1.2rem] font-[500] leading-[1.5rem]">
 								Really boy law county she unable her sister.
 								Feet you off its like like six. Among sex are
 								leave law built now. In built table in an rapid
